44fac5df9860adc83e008bb550b55a925004f619
Yamaç Demirkan Yılmaz <yamac.demirkan@protonmail.ch>
2020-12-11 21:37:16 +0300

    Updated Panel.java

--- CS320_Fall2020_Project_GUI/src/GraphicalUserInterface/Panel.java
+++ CS320_Fall2020_Project_GUI/src/GraphicalUserInterface/Panel.java
@@ -10,49 +10,53 @@ public class Panel {
 
     public void initializePanels() {
         JPanel imgElementPanel = new JPanel();
-        imgElementPanel.add(new JTextField());
-        imgElementPanel.add(new JTextField());
-        imgElementPanel.add(new JTextField());
+        imgElementPanel.add(new JTextField("src"));
+        imgElementPanel.add(new JTextField("width"));
+        imgElementPanel.add(new JTextField("height"));
         panels.add(imgElementPanel);
 
         JPanel inputElementPanel = new JPanel();
-        inputElementPanel.add(new JTextField());
-        inputElementPanel.add(new JTextField());
-        inputElementPanel.add(new JTextField());
-        inputElementPanel.add(new JTextField());
-        inputElementPanel.add(new JTextField());
+        inputElementPanel.add(new JTextField("type"));
+        inputElementPanel.add(new JTextField("value"));
+        inputElementPanel.add(new JCheckBox("readonly"));
+        inputElementPanel.add(new JTextField("placeholder"));
+        inputElementPanel.add(new JCheckBox("required"));
         panels.add(inputElementPanel);
 
         JPanel labelElementPanel = new JPanel();
-        labelElementPanel.add(new JTextField());
-        labelElementPanel.add(new JTextField());
+        labelElementPanel.add(new JTextField("forinput"));
+        labelElementPanel.add(new JTextField("text"));
         panels.add(labelElementPanel);
 
         JPanel buttonElementPanel = new JPanel();
-        buttonElementPanel.add(new JTextField());
-        buttonElementPanel.add(new JTextField());
-        buttonElementPanel.add(new JTextField());
-        buttonElementPanel.add(new JTextField());
+        buttonElementPanel.add(new JCheckBox("autofocus"));
+        buttonElementPanel.add(new JCheckBox("disabled"));
+        buttonElementPanel.add(new JTextField("type"));
+        buttonElementPanel.add(new JTextField("text"));
         panels.add(buttonElementPanel);
 
         JPanel pElementPanel = new JPanel();
-        pElementPanel.add(new JTextField());
+        pElementPanel.add(new JTextField("text"));
         panels.add(pElementPanel);
 
         JPanel aElementPanel = new JPanel();
-        aElementPanel.add(new JTextField());
+        aElementPanel.add(new JTextField("href"));
         panels.add(aElementPanel);
 
         JPanel h1ElementPanel = new JPanel();
-        h1ElementPanel.add(new JTextField());
+        h1ElementPanel.add(new JTextField("text"));
         panels.add(h1ElementPanel);
 
         JPanel ulElementPanel = new JPanel();
-        ulElementPanel.add(new JTextField());
+        for(int i = 0; i < 10; i++) {
+            ulElementPanel.add(new JTextField("item"));
+        }
         panels.add(ulElementPanel);
 
         JPanel olElementPanel = new JPanel();
-        olElementPanel.add(new JTextField());
+        for(int i = 0; i < 10; i++) {
+            olElementPanel.add(new JTextField("item"));
+        }
         panels.add(olElementPanel);
     }
 
