9361390bc1eb863c8310fef61ca1844d34cc6c9f
Hasan <hasan.bilgin@ozu.edu.tr>
2020-12-17 16:59:15 +0300

    added ulElement class

---
+++ HTML_Elements/src/ulElement.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class ulElement implements Element {
+    private String text = "<ul>\n" +
+            "</ul><br>\n";
+
+    public ulElement() { }
+
+    public ulElement(String item) {
+        addItem(item);
+    }
+
+    public ulElement(ArrayList<String> arr) {
+        for (String item : arr)
+            addItem(item);
+    }
+
+    public void addItem(String item) {
+        int index = text.indexOf("</ul>");
+        StringBuilder newString = new StringBuilder(text);
+        newString.insert(index, "<li>" + item + "</li>\n");
+        text = newString.toString();
+    }
+
+    @Override
+    public String getText() {
+        return text;
+    }
+}
