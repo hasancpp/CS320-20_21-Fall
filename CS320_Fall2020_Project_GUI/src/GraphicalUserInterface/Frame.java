c5a899e126c4220ce61839ded73f6bec07bc4154
Yamaç Demirkan Yılmaz <yamac.demirkan@protonmail.ch>
2020-12-19 22:11:56 +0300

    Updated the GUI to satisfy the requirements.

--- CS320_Fall2020_Project_GUI/src/GraphicalUserInterface/Frame.java
+++ CS320_Fall2020_Project_GUI/src/GraphicalUserInterface/Frame.java
@@ -1,5 +1,5 @@
 package GraphicalUserInterface;
-import java.awt.GridLayout;
+import java.awt.*;
 
 import javax.swing.*;
 
@@ -7,13 +7,13 @@ import javax.swing.*;
 // Nebi Peker Celik && Yamaç Demirkan Yılmaz -> v0.2
 // Contains the implementation of Frame class in Graphical User Interface package.
 
-public class Frame implements GraphicalUserInterface {
+public class Frame extends JFrame implements GraphicalUserInterface {
 
     private JFrame frame;
 
     public Frame() {
         frame = new JFrame("JWPL Software");
-        frame.setLayout(new GridLayout(3,3));
+        frame.setLayout(new BorderLayout());
         frame.setSize(1200, 700);
         frame.setLocationByPlatform(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,6 +25,6 @@ public class Frame implements GraphicalUserInterface {
 
     @Override
     public void add(JComponent component) {
-        frame.add(component);
+        // ???
     }
 }
--- CS320_Fall2020_Project_GUI/src/GraphicalUserInterface/main.java
+++ CS320_Fall2020_Project_GUI/src/GraphicalUserInterface/main.java
@@ -1,24 +1,27 @@
 package GraphicalUserInterface;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.Map;
 
 public class main {
 
     public static void main(String[] args) {
         Frame myframe = new Frame();
-        Panel mypanel = new Panel();
-        mypanel.initializePanels();
-        Button mybutton = new Button();
-        mybutton.initializeButton();
-
-        for(Map.Entry<String, JButton> set : mybutton.getButtonHashMap().entrySet()) {
-            myframe.getFrame().add(set.getValue());
+        JPanel toppanel = new JPanel();
+        myframe.getFrame().add(toppanel, BorderLayout.NORTH);
+        JPanel centerpanel = new JPanel();
+        myframe.getFrame().add(centerpanel, BorderLayout.CENTER);
+        JPanel bottompanel = new JPanel();
+        myframe.getFrame().add(bottompanel, BorderLayout.SOUTH);
+        JPanel buttonpanel = new JPanel();
+        buttonpanel.setLayout(new GridLayout(9,1));
+        Button mybuttons = new Button();
+        mybuttons.initializeButton();
+        for(Map.Entry<String, JButton> set : mybuttons.getButtonHashMap().entrySet()) {
+            buttonpanel.add(set.getValue());
         }
-
-        for(int i=0; i < mypanel.getPanels().size(); i++) {
-            myframe.getFrame().add(mypanel.getPanels().get(i));
-        }
+        myframe.getFrame().add(buttonpanel, BorderLayout.EAST);
 
         myframe.getFrame().pack();
         myframe.getFrame().setVisible(true);
